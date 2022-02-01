7c899e41a94e4b645cd6c4223df2a7f550b21b87
P. Sinha <pnsinha@uchicago.edu>
2022-02-01 13:39:12 -0600

    updated Rcode

--- Rcode_geocoding.R
+++ Rcode_geocoding.R
@@ -1,13 +1,15 @@
 #Geocoding Workshop
+library(pacman)
 #load up the ggmap library
-library(ggmap)
+p_load(ggmap)
 #https://maps.googleapis.com/maps/api/geocode/json?address=baylor+university&key=xxx
-library(tidyverse)
-library(tmaptools)
+p_load(tidyverse)
+p_load(tmaptools)
 
-setwd("D:\\Dropbox\\4#Research\\Uchicago\\WORKSHOP\\geocoding")
+setwd("F:\\GitHub\\geocoding")
 
-gkey = "AIzaSyCa9_mrD7EwhR5MUJx_uX62O0CiXoVLUaw"
+#gkey = "AIzaSyCa9_mrD7EwhR5MUJx_uX62O0CiXoVLUaw"
+
 # save api key
 register_google(key = gkey)
 
@@ -22,6 +24,7 @@ data<-mutate(data, addr=paste(ADDRESS, CITY,  STATE, ZIPCODE, sep=','))
 gcode<-mutate_geocode(data, location=addr)
 
 #geocode using OSM
+#Similar to geocode from the ggmap package. It uses OpenStreetMap Nominatim. 
 osmcode<-geocode_OSM(data[,"addr"])
 
 
@@ -53,4 +56,24 @@ cmbdloc<-cmbdloc %>% rowwise() %>% mutate(google_esri=get_geo_distance(lon.googl
 #check the mean deviation
 summary(cmbdloc$google_esri)
 summary(cmbdloc$osm_esri)
+
+#################################
+#OpenStreetMap-Based Routing Service
+#################################
+
+p_load(osrm)
+#Reading file from my box drive, these are 10 points (long, lat) in Germany
+pts<-read.csv("https://uchicago.box.com/shared/static/sxg1hlb7vv4mmtrq3uzvxy2eujl7ckgh.csv")
+
+#calculate drive time table
+osrmTable(loc = pts)
+
+#calculate drive time between source and destination
+osrmTable(src = pts[1:5,], dst = pts[6:10,])
+
+#calculate drive time and distance between source and destination
+osrmTable(src = pts[1:5,], dst = pts[6:10,], measure = c('duration', 'distance'))
+
+#For drive time and distance between only two points (eg. between 1 and 6)
+osrmRoute(src = pts[1,], dst = pts[6,], overview=FALSE)
 
